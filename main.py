04ce3ec169747b2e7f58b4a3ee41e3175998569b
Sven Wanzenried <sven.wanzenried@gmail.com>
2022-12-19 06:37:39 +0100

    Improve main.py

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ with open('secrets.json', 'r') as f:
     api_key_json = json.load(f)
     
 client = vimeo.VimeoClient(
-    token=api_key_json["token"],
-    key=api_key_json["client_id"],
-    secret=api_key_json["secret"]
+    token=api_key_json["vimeo_token"],
+    key=api_key_json["vimeo_client_id"],
+    secret=api_key_json["vimeo_secret"]
 )
 
 request_args = {
-    'per_page': 2,
-    'filter': 'nolive',
-    'query_fields': 'type',
-    'query': 'live',
-    # 'fields': 'uri,name,link,created_time,modified_time,privacy.view',
-    'sort': 'modified_time',
+    'per_page': 10,
+    # 'filter': 'nolive',
+    'query_fields': 'description',
+    'query': 'test',
+    'fields': 'uri, link, name, type, duration, modified_time, created_time, privacy.view',
+    'sort': 'date',
     'direction': 'desc'
 }
 
@@ -25,5 +25,6 @@ resp = client.get(f"/me/videos/?{urlencode(request_args)}")
 resp_json = json.loads(json.dumps(resp.json()))
 with open('log.txt','w') as f:
     f.write(json.dumps(resp_json, indent=4))
+print(f"Found videos: { len(resp_json['data']) }")
 # print(datetime.fromisoformat(resp.json()['data']['modified_time']))
 d = dict(resp.json())
